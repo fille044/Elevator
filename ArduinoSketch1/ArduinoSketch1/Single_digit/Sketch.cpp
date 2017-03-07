416b44710371d0d4917f6f238c53260b303103c4
Unknown <fille044@gmail.com>
2017-03-07 12:59:19 +0100

    yes

--- ArduinoSketch1/ArduinoSketch1/Single_digit/Sketch.cpp
+++ ArduinoSketch1/ArduinoSketch1/Single_digit/Sketch.cpp
@@ -7,18 +7,25 @@
 #define button_Y 0
 #define button_R 1
 #define button_B 2
+#define button_hiss_0 3
+#define button_hiss_1 4
+#define button_hiss_2 5
+
 
 void setup() {
 	pinMode(led_Y,OUTPUT);
-	pinMode(button_Y, INPUT);	
 	pinMode(led_R,OUTPUT);
-	pinMode(button_R, INPUT);	
 	pinMode(led_B,OUTPUT);
+	pinMode(button_Y, INPUT);	
+	pinMode(button_R, INPUT);	
 	pinMode(button_B, INPUT);
+	pinMode(button_hiss_0, INPUT);
+	pinMode(button_hiss_1, INPUT);
+	pinMode(button_hiss_2, INPUT);
 	Serial.begin(9600);
 }
 
-void loop() {
+void light_it_up(void){
 	static int i = 0;
 	//Clear LEDs
 	digitalWrite(led_Y,0);
@@ -30,18 +37,18 @@ void loop() {
 	}
 	if (i == 1){
 		digitalWrite(led_R,1);
-	}	
+	}
 	if (i == 2){
 		digitalWrite(led_B,1);
 	}
-	
+
 	// All combinations for where you and the elevetor are
 	// 3s delay for one floor, 6s delay for two floors
-	if (digitalRead(button_Y) == 0 && i == 2){
+	if ((digitalRead(button_Y) == 0 || digitalRead(button_hiss_0)==0) && i == 2){
 		i = 0;
 		digitalWrite(led_Y,1);
 		for (int jx = 0; jx <100; jx++){
-			if (digitalRead(button_R) == 0){
+			if ((digitalRead(button_R) == 0 || digitalRead(button_hiss_1)==0)){
 				digitalWrite(led_R,1);
 				delay(1000);
 				digitalWrite(led_B,0);
@@ -50,26 +57,26 @@ void loop() {
 		}
 		delay(3000);
 	}
-	if (digitalRead(button_Y) == 0 && i == 1){
+	if ((digitalRead(button_Y)==0 || digitalRead(button_hiss_0)==0) && i == 1){
 		i = 0;
 		digitalWrite(led_Y,1);
 		delay(3000);
 	}
-	if (digitalRead(button_R) == 0 && i == 0){
+	if ((digitalRead(button_R)==0 || digitalRead(button_hiss_1)==0) && i == 0){
 		i = 1;
 		digitalWrite(led_R,1);
 		delay(3000);
 	}
-	if (digitalRead(button_R) == 0 && i == 2){
+	if ((digitalRead(button_R)==0 || digitalRead(button_hiss_1)==0) && i == 2){
 		i = 1;
 		digitalWrite(led_R,1);
 		delay(3000);
 	}
-	if (digitalRead(button_B) == 0 && i == 0){
+	if ((digitalRead(button_B)==0 || digitalRead(button_hiss_2)==0) && i == 0){
 		i = 2;
 		digitalWrite(led_B,1);
 		for (int jx = 0; jx <100; jx++){
-			if (digitalRead(button_R) == 0){
+			if ((digitalRead(button_R) == 0 || digitalRead(button_hiss_1)==0)){
 				digitalWrite(led_R,1);
 				delay(1000);
 				digitalWrite(led_Y,0);
@@ -78,9 +85,13 @@ void loop() {
 		}
 		delay(3000);
 	}
-	if (digitalRead(button_B) == 0 && i == 1){
+	if ((digitalRead(button_B)==0 || digitalRead(button_hiss_2)==0) && i == 1){
 		i = 2;
 		digitalWrite(led_B,1);
 		delay(3000);
 	}
+}
+
+void loop() {
+	light_it_up();
 }
---
+++ ArduinoSketch1/ArduinoSketch1/Single_digit/functions.cpp
@@ -0,0 +1,6 @@
+/*
+ * functions.cpp
+ *
+ * Created: 07/03/2017 12:52:39
+ *  Author: fille
+ */ 
